5729edc4bf5459b683bbb3d433a7c96d9b82b80e
Cedric Kiplimo <kiplimock@gmail.com>
2022-01-31 12:50:51 +0300

    added reprojection error scatter plot

--- single_calibration.py
+++ single_calibration.py
@@ -1,9 +1,11 @@
  # this script performs the calibration for a   single camera
-from sys import flags
+import random
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 import glob
 from param_storage import  save_coefficients
+from turtle import color
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -26,6 +28,7 @@ def calibrate(path, square_size, width=9, height=6):
 
     # loop through all the images
     # discard image if opencv cannot find corners
+    global images
     images = glob.glob(path + '/' + "*.jpg")
 
     for fname in images:
@@ -75,17 +78,60 @@ def projection_error(objpoints, imgpoints, tvecs, rvecs, mtx, dist):
 
         # compute error between projected points and original points
         error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-        mean_error += error**2
+        mean_error += error
     
+    # print(len(objpoints))
+    # print(imgpoints[0][1])
     return mean_error/len(objpoints)
+
+def plot_scatter(objpoints, imgpoints, tvecs, rvecs, mtx, dist):
+    """
+    This function creates a scatterplot of the residual errors
+    due to differences between original and reprojected image points
+    """
+    # initialize list of errors
+    errors = []
+    x = [] # list of x ordinates
+    y = [] # list of y ordinates
+
+
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist) 
+        # --> len(imgpoints2) = 54
+        error = [np.subtract(imgpoints2[j][0], imgpoints[i][j][0]) for j in range(len(imgpoints2))] # --> [x,y]
+        errors.append(error) # --> [[x1,y1], [x2,y2], ..., [xn,yn]]
+    
+        x.append([pair[0] for pair in error])
+        y.append([pair[1] for pair in error])
+        
+    # plot figure
+    plt.figure(figsize=(10, 6))
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Reprojection Errors in Pixels")
+
+    labels = ["Image " + str(i) for i in range(len(images))]
+    colors = ["#" + ''.join([random.choice('0123456789ABCDEF') for i in range(6)]) for j in range(len(imgpoints))]
+    for i in range(len(colors)):
+        # different colour for each image
+        # labels and colours correspond
+        plt.scatter(x[i], y[i], marker='+', color=colors[i], label=[labels[i]])
+    
+    # plot legend outside the image
+    plt.legend(bbox_to_anchor=(1.05,1.0), loc='upper left')
+    plt.tight_layout()
+    plt.show()
+
+
 
 
 if __name__ == "__main__":
 
-    ret, mtx, dist, rvecs, tvecs, image_points, object_points = calibrate(path='images/cam1/take5', square_size=2.3)
+    ret, mtx, dist, rvecs, tvecs, image_points, object_points = calibrate(path='images/cam1/take1', square_size=2.3)
 
     save_coefficients(mtx, dist, tvecs[0], rvecs[0], 'cam.yml')
     p_error = projection_error(object_points, image_points, tvecs, rvecs, mtx, dist)
 
     print("Calibration is finished. RMS: ", ret)
     print("Mean Projection Error: ", p_error)
+    plot_scatter(object_points, image_points, tvecs, rvecs, mtx, dist)
